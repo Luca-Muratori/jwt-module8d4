952cac91a0bd4dd0acf9a38729ab787dc54711b9
Lucai994 <luca.muratori.94@hotmail.it>
2022-05-26 13:36:59 +0200

    commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,10 +7,7 @@ const Homepage = () => {
         This is a simple hero unit, a simple jumbotron-style component for
         calling extra attention to featured content or information.
       </p>
-      <p>
-        <Button variant="primary">Sign in</Button>
-        <Button variant="secondary">Sign up</Button>
-      </p>
+      <p></p>
     </Jumbotron>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Navbar, Nav, Button } from "react-bootstrap";
+
+const NavbarComponent = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Button variant="primary">Sign in</Button>
+          <Button variant="secondary">Sign up</Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
