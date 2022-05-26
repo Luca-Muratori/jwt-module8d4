04f8406a3200384c7908e2d2a0452fd0359ad96c
Lucai994 <luca.muratori.94@hotmail.it>
2022-05-26 14:01:31 +0200

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NavbarComponent = () => {
   return (
@@ -6,8 +7,13 @@ const NavbarComponent = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Button variant="primary">Sign in</Button>
-          <Button variant="secondary">Sign up</Button>
+          <Link to="/register">
+            {" "}
+            <Button variant="primary">Sign up</Button>
+          </Link>
+          <Link to="/login">
+            <Button variant="secondary">Sign in</Button>
+          </Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
